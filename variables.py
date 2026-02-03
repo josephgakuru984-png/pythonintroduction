ceba4921a8895bd8ddb39364d1d286699e74a984
josephgakuru984-png <josephgakuru984@gmail.com>
2026-02-03 13:44:29 +0300

    initial commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+# variable is a container used to store a value
+
+school = "emobilis"
+firstname = "John"
+lastname = "Smith"
+num1 =45
+num2 =100
+language = "python"
+print(num1+num2)
+print(school)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(school, "is a coding school")
+print(language, "is a web programing language")
+print("the sum of", num1, "and",num2, "is",num1+num2)
+
+
