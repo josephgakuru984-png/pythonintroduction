374c7ec0109acc6c221cd30c9fd1d2007c5e4ae2
josephgakuru984-png <josephgakuru984@gmail.com>
2026-02-11 12:59:34 +0300

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
 x = float(input("enter"))
+
+first = int(input("Enter first number"))
+operator = input("Enter the operator")
+second = int(input("Enter first number"))
+
+if operator == "+":
+    print(first + second)
+
+if operator == "-":
+    print(first / second)
+
+if operator == "/":
+    print(first / second)
+
+if operator == "*":
+    print(first * second)
+
+else:
+    print("you have entered an invalid operator")
+
+
+
+
